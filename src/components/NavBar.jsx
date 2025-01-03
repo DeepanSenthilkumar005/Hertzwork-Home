5a2d29878b2888c54d4e42617e1a03a7a47b0cbb
DeepanSenthilkumar005 <deepancse45@gmail.com>
2025-01-03 19:57:51 +0530

    fix:NavBar css changed to tailwind

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import './NavBar.css';
-import logo from '../assets/logo.jpg'
+import logo from '../assets/logo.jpg';
 
-function NavBar(){
-    return(
-        <div className='navbar'>
-            <img src={logo} alt='Logo' className='logo'/>
-            <ul className='list'>
-                <li>Home</li>
-                <li>Services</li>
-                <li>Products</li>
-                <li>Career</li>
-                <li>Contact Us</li>
-            </ul>
+function NavBar() {
+  return (
+    <div className="navbar w-full flex items-center justify-between bg-black py-[12.5px] px-8 text-white">
+      <img src={logo} alt="Logo" className="logo w-36 cursor-pointer ml-5" />
 
-            <div className='enquiry'>
-                <p>Enquire Now</p>
-            </div>
+      <ul className="list w-full justify-center flex list-none text-center p-0 flex-1">
+        <li className="inline-block my-[10px] mx-5 text-base cursor-pointer font-sans py-[12.5px] px-[15px] border border-transparent transition-all duration-300 ease-in-out hover:border-[#262626] hover:rounded-md hover:text-[#fe5d26]">
+          Home
+        </li>
+        <li className="inline-block my-[10px] mx-5 text-base cursor-pointer font-sans py-[12.5px] px-[15px] border border-transparent transition-all duration-300 ease-in-out hover:border-[#262626] hover:rounded-md hover:text-[#fe5d26]">
+          Services
+        </li>
+        <li className="inline-block my-[10px] mx-5 text-base cursor-pointer font-sans py-[12.5px] px-[15px] border border-transparent transition-all duration-300 ease-in-out hover:border-[#262626] hover:rounded-md hover:text-[#fe5d26]">
+          Products
+        </li>
+        <li className="inline-block my-[10px] mx-5 text-base cursor-pointer font-sans py-[12.5px] px-[15px] border border-transparent transition-all duration-300 ease-in-out hover:border-[#262626] hover:rounded-md hover:text-[#fe5d26]">
+          Career
+        </li>
+        <li className="inline-block my-[10px] mx-5 text-base cursor-pointer font-sans py-[12.5px] px-[15px] border border-transparent transition-all duration-300 ease-in-out hover:border-[#262626] hover:rounded-md hover:text-[#fe5d26]">
+          Contact Us
+        </li>
+      </ul>
 
-        </div>
-    )
+      <div className="enquiry py-[10px] px-[15px] text-[14px] border border-transparent rounded-[6.25px] font-urbanist transition-all duration-300 ease-in-out cursor-pointer hover:border-[#fe5d26] hover:bg-[#262626]">
+        <p>Enquire Now</p>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,16 +1,29 @@
 import './WelcomePage.css';
 function WelcomePage() {
   return (
-    <div className="welcomePage bg-black min-h-screen w-screen grid md:grid-cols-2 text-center">
-        <div className="leftcontent md:col-span-1 h-3/5 my-auto p-7 px-11 ms-20 rounded-md rounded-br-3xl bg-darkGray">
-            <h2 className="heading mt-5 text-white text-4xl">Welcome to <span className='text-orange-600'>Hertzworkz!</span></h2>
-            <p className='text-welcometext text-xl text-left mt-8'>Discover a world where innovation meets excellence. At Hertzworkz, we are dedicated to delivering cutting-edge solutions that empower businesses to thrive in a dynamic landscape. With a passion for design, development, and advanced technologies, we create impactful experiences that drive success. Join us on our journey to shape the future and make a meaningful difference together.</p>
-        </div>
-        <div className="rightContent">
+    <section className="welcomePage bg-black w-screen text-center grid md:grid-rows-4">
+        {/* Top Content */}
+        <div className="topContent md:row-span-3 min-h-screen grid md:grid-cols-2">
+            {/* Left Content */}
+            <div className="leftcontent md:col-span-1 h-fit my-auto p-7 px-11 ms-20 rounded-md rounded-br-3xl bg-darkGray">
+                <h2 className="heading mt-5 text-white text-4xl">Welcome to <span className='text-orange-600'>Hertzworkz!</span></h2>
+                <p className='text-welcometext text-xl text-left mt-8'>Discover a world where innovation meets excellence. At Hertzworkz, we are dedicated to delivering cutting-edge solutions that empower businesses to thrive in a dynamic landscape. With a passion for design, development, and advanced technologies, we create impactful experiences that drive success. Join us on our journey to shape the future and make a meaningful difference together.</p>
+            </div>
+            {/* Right Content */}
+            <div className="rightContent md:row-span-1">
 
+            </div>
+        </div>
+        {/* Buttom Content */}
+        <div className="buttomContent bg-darkGray m-4 rounded-md h-4/5 text-white">
+                <div>
+                    <span></span>
+                    <i></i>
+                    <p>Innovtive Design</p>
+                </div>
         </div>
 
-    </div>
+    </section>
   )
 }
 
