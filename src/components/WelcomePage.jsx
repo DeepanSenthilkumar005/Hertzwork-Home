f91f4df2a3de3dd47610f928b26cbe37d7c8e0d4
DeepanSenthilkumar005 <deepancse45@gmail.com>
2025-01-03 20:02:31 +0530

    fix:Welcome css changed to tailwind

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -5,7 +5,7 @@ function WelcomePage() {
         {/* Top Content */}
         <div className="topContent md:row-span-3 min-h-screen grid md:grid-cols-2">
             {/* Left Content */}
-            <div className="leftcontent md:col-span-1 h-fit my-auto p-7 px-11 ms-20 rounded-md rounded-br-3xl bg-darkGray">
+            <div className="leftcontent rounded-br-[4rem] md:col-span-1 h-fit my-auto p-7 px-11 ms-20 rounded-md bg-darkGray">
                 <h2 className="heading mt-5 text-white text-4xl">Welcome to <span className='text-orange-600'>Hertzworkz!</span></h2>
                 <p className='text-welcometext text-xl text-left mt-8'>Discover a world where innovation meets excellence. At Hertzworkz, we are dedicated to delivering cutting-edge solutions that empower businesses to thrive in a dynamic landscape. With a passion for design, development, and advanced technologies, we create impactful experiences that drive success. Join us on our journey to shape the future and make a meaningful difference together.</p>
             </div>
