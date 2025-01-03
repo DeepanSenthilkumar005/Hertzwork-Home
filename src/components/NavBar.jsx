2e6d807d5135fe880dbc1c7cda66d3322ea7eb56
DeepanSenthilkumar005 <deepancse45@gmail.com>
2025-01-03 20:36:56 +0530

    fix:responsive design for navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoCloseOutline } from "react-icons/io5";
 import logo from '../assets/logo.jpg';
 
 function NavBar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <div className="navbar w-screen flex items-center justify-between bg-black py-[12.5px] px-8 text-white">
-      <img src={logo} alt="Logo" className="logo w-36 cursor-pointer ml-5" />
+      <img src={logo} alt="Logo" className="logo stm:w-20 ms-0 md:w-36 cursor-pointer ml-5" />
 
+      <div className='stm:hidden'>
       <ul className="list w-full justify-center flex list-none text-center p-0 flex-1">
         <li className="inline-block my-[10px] mx-5 text-base cursor-pointer font-sans py-[12.5px] px-[15px] border border-transparent transition-all duration-300 ease-in-out hover:border-[#262626] hover:rounded-md hover:text-[#fe5d26]">
           Home
@@ -26,6 +30,32 @@ function NavBar() {
 
       <div className="enquiry py-[10px] px-[15px] text-[14px] border border-transparent rounded-[6.25px] font-urbanist transition-all duration-300 ease-in-out cursor-pointer hover:border-[#fe5d26] hover:bg-[#262626]">
         <p>Enquire Now</p>
+      </div>
+      </div>
+      <div className='md:hidden'>
+        <div className="button" onClick={() => setIsMenuOpen(!isMenuOpen)} >
+          {(!isMenuOpen)?(<RxHamburgerMenu />):(<IoCloseOutline className='text-white ms-auto' />)}
+        </div>
+        
+
+        <ul className={`flex flex-col md:flex-row md:justify-start p-0 list-none text-center ${isMenuOpen ? 'block' : 'hidden'}`}>
+        <li className="inline-block my-[10px] mx-5 text-base cursor-pointer font-sans py-[12.5px] px-[15px] border border-transparent transition-all duration-300 ease-in-out hover:border-[#262626] hover:rounded-md hover:text-[#fe5d26]">
+          Home
+        </li>
+        <li className="inline-block my-[10px] mx-5 text-base cursor-pointer font-sans py-[12.5px] px-[15px] border border-transparent transition-all duration-300 ease-in-out hover:border-[#262626] hover:rounded-md hover:text-[#fe5d26]">
+          Services
+        </li>
+        <li className="inline-block my-[10px] mx-5 text-base cursor-pointer font-sans py-[12.5px] px-[15px] border border-transparent transition-all duration-300 ease-in-out hover:border-[#262626] hover:rounded-md hover:text-[#fe5d26]">
+          Products
+        </li>
+        <li className="inline-block my-[10px] mx-5 text-base cursor-pointer font-sans py-[12.5px] px-[15px] border border-transparent transition-all duration-300 ease-in-out hover:border-[#262626] hover:rounded-md hover:text-[#fe5d26]">
+          Career
+        </li>
+        <li className="inline-block my-[10px] mx-5 text-base cursor-pointer font-sans py-[12.5px] px-[15px] border border-transparent transition-all duration-300 ease-in-out hover:border-[#262626] hover:rounded-md hover:text-[#fe5d26]">
+          Contact Us
+        </li>
+      </ul>
+
       </div>
     </div>
   );
--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,22 +1,36 @@
-import './WelcomePage.css';
 function WelcomePage() {
   return (
     <section className="welcomePage bg-black w-screen text-center grid md:grid-rows-4">
         {/* Top Content */}
-        <div className="topContent md:row-span-3 min-h-screen grid md:grid-cols-2">
+        <div className="topContent md:row-span-3 p-0 m-0 w-full min-h-screen grid md:grid-cols-2">
             {/* Left Content */}
-            <div className="leftcontent rounded-br-[4rem] md:col-span-1 h-fit my-auto p-7 px-11 ms-20 rounded-md bg-darkGray">
+            <div className="leftcontent justify-center rounded-br-[4rem] md:col-span-1 h-fit my-auto p-7 px-11 ms-20 rounded-md bg-darkGray">
                 <h2 className="heading mt-5 text-white text-4xl">Welcome to <span className='text-orange-600'>Hertzworkz!</span></h2>
                 <p className='text-welcometext text-xl text-left mt-8'>Discover a world where innovation meets excellence. At Hertzworkz, we are dedicated to delivering cutting-edge solutions that empower businesses to thrive in a dynamic landscape. With a passion for design, development, and advanced technologies, we create impactful experiences that drive success. Join us on our journey to shape the future and make a meaningful difference together.</p>
             </div>
             {/* Right Content */}
             <div className="rightContent md:row-span-1">
-
+                {/* Image comes here */}
             </div>
         </div>
         {/* Buttom Content */}
-        <div className="buttomContent bg-darkGray m-4 rounded-md h-4/5 text-white">
-                <div>
+        <div className="buttomContent grid stm:grid-cols-2 md:grid-cols-4 space-x-3 space-y-3 p-2 bg-[#313132] m-4 rounded-md h-4/5 text-welcometext">
+                  <div className="bg-[#1a1a1a] rounded-md my-2 hover:scale-105 duration-200 ease-in-out">
+                    <span></span>
+                    <i></i>
+                    <p>Innovtive Design</p>
+                </div>              
+                <div className="bg-[#1a1a1a] rounded-md my-2 hover:scale-105 duration-200 ease-in-out">
+                    <span></span>
+                    <i></i>
+                    <p>Innovtive Design</p>
+                </div>              
+                <div className="bg-[#1a1a1a] rounded-md my-2 hover:scale-105 duration-200 ease-in-out">
+                    <span></span>
+                    <i></i>
+                    <p>Innovtive Design</p>
+                </div>              
+                <div className="bg-[#1a1a1a] rounded-md my-2 hover:scale-105 duration-200 ease-in-out">
                     <span></span>
                     <i></i>
                     <p>Innovtive Design</p>
