76d09ae90000befa10e5819d2af5544fd683c40a
DeepanSenthilkumar005 <deepancse45@gmail.com>
2025-01-10 18:22:25 +0530

    fix:Feature Page Details Added

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Features() {
 
     const List = ["Online Solutions", "Business Tools", "Customer Support"];
-  const cards = [
+  const cards1= [
     {
       title: "Web Development Services",
       description:
@@ -26,16 +26,62 @@ function Features() {
         "Share, communicate, and collaborate in real-time, no matter where you are.",
     },
   ];
+  const cards2= [
+    {
+      title: "Analytics & Reporting",
+      description:
+        "Tools for tracking your business metrics, analyzing trends, and reporting on performance.",
+    },
+    {
+      title: "Financial Tools",
+      description:
+        "Manage budgets, expenses, and transactions, helping to keep finances in check.",
+    },
+    {
+      title: "Project Management Tools",
+      description:
+        "Coordinate tasks, track progress, and manage deadlines to ensure your team stays productive.",
+    },
+    {
+      title: "AI/ML Solutions",
+      description:
+        "Use intelligent tools to automate tasks, predict trends, and optimize business strategies.",
+    },
+  ];
+  const cards3= [
+    {
+      title: "24/7 Technical Support",
+      description:
+        "Get round-the-clock assistance with any technical challenges or system issues.",
+    },
+    {
+      title: "Helpdesk & Ticketing",
+      description:
+        "A dedicated helpdesk to resolve queries and manage service requests efficiently.",
+    },
+    {
+      title: "Customer Success Team",
+      description:
+        "Guidance to help clients maximize the potential of their services or products.",
+    },
+    {
+      title: "Training and Onboarding",
+      description:
+        "Personalized support to help you and your team get up to speed with new systems and tools.",
+    },
+  ];    
 
     return(
       <section className='Feature page bg-black w-screen h-screen'>
         {/*Top content*/}
         <div className='topContent'>
-        <p className="text-4xl font-bold mb-2 text-left text-white px-8">Our
-        <span className='text-4xl font-bold text-orange-600'> Features</span> 
-        </p> 
-        <p className='text-2xl text-white tracking-widest px-8'>Discover a wide range of powerful features at Hertzworkz, designed to streamline your business operations and enhance productivity. From innovative solutions to seamless user experiences, we provide tools that empower growth and success.</p>
+          <p className="text-4xl font-bold mb-2 text-left text-white px-8">Our
+            <span className='text-4xl font-bold text-orange-600'> Features</span> 
+          </p> 
+          <p className='text-2xl text-white tracking-widest px-8'>Discover a wide range of powerful features at Hertzworkz, designed to streamline your business operations and enhance productivity. From innovative solutions to seamless user experiences, we provide tools that empower growth and success.</p>
         </div>
+        {/* Bottom Content */}
+
 
         
 
